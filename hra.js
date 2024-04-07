b22ded2727bb383455af4f8114b46e2d74876e00
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-04-07 21:12:47 +0200

    test-commit

---
+++ hra.js
@@ -0,0 +1,45 @@
+let currentPlayer = 'circle';
+
+const gamePlay = document.querySelector('.game-play');
+const playerTurnDisplay = document.querySelector('.player');
+
+function handleButtonClick(event) {
+  const btn = event.target;
+  if (!btn.classList.contains('button-clicked')) {
+    if (currentPlayer === 'circle') {
+      btn.classList.add('game-play--circle', 'animate__animated', 'animate__zoomIn', 'animate__faster');
+      currentPlayer = 'cross';
+    } else {
+      btn.classList.add('game-play--cross', 'animate__animated', 'animate__zoomIn', 'animate__faster');
+      currentPlayer = 'circle';
+    }
+    btn.classList.add('button-clicked');
+    updatePlayerTurnDisplay();
+  }
+}
+
+function updatePlayerTurnDisplay() {
+  const playerImage = document.querySelector('.player-image');
+  playerImage.src = `./svg/${currentPlayer}.svg`;
+  playerImage.alt = currentPlayer;
+}
+
+document.querySelectorAll('.button').forEach(button => {
+  button.addEventListener('click', handleButtonClick);
+});
+
+// Updating the display of the current player after the page is loaded Оновлення відображення поточного гравця після завантаження сторінки
+updatePlayerTurnDisplay();
+
+// BONUS
+
+const restart = document.querySelector('.restart');
+restart.addEventListener('click', (e) => {
+  const confirmed = confirm('Are you sure you want to restart the game?');
+  if (!confirmed) {
+    e.preventDefault(); 
+  } else {
+    console.log('Restarting the game...');
+  }
+});
+
